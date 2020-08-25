38d41e7f4ddb50310d0267ca38287532696de307
ch15aki <and.1la.view@gmail.com>
2020-08-25 21:23:57 +0900

    SNS認証の機能を実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
+  def facebook
+    authorization
+  end
+
+  def google_oauth2
+    authorization
+  end
   # You should also create an action method in this controller like this:
   # def twitter
   # end
@@ -21,6 +28,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super
   # end
 
+  private
+
+  def authorization
+    sns_info = User.from_omniauth(request.env["omniauth.auth"])
+    @user = sns_info[:user]
+
+    if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
+      sign_in_and_redirect @user, event: :authentication
+    else #ユーザー情報が未登録なので、新規登録画面へ遷移する
+      @sns_id = sns_info[:sns].id
+      render template: 'devise/registrations/new'
+    end
+
+  end
   # protected
 
   # The path used when OmniAuth fails
